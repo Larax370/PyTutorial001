db5ab127dc2e5fb4c079e614e5ce4bc76cd882b5
Larax370 <Larax37@gmail.com>
2020-05-30 20:24:14 -0400

    Commit ElCairo 20200530

--- Curso03.py
+++ Curso03.py
@@ -1,10 +1,12 @@
-print "Hola Python"
-e = 5
-e = 5L
+print ("Hola Python")
+a = 2
+b = 5
+Print (a/b)
+
 real = 0.5645
 real = .5645e3
 
-print real
+print (real)
 
 a = 26
 b = 11.3
@@ -12,32 +14,32 @@ c = 5
 d = 3.5
 
 #Suma
-print a+b
+print (a+b)
 #exponente
-print c**2
+print (c**2)
 #Division obligando a resultado float
-print float(c)/a
+print (float(c)/a)
 #Modulo
-print 7%3
+print (7%3)
 
 #Cadenas
 cads = 'Texto entre \n comillas \tsimples'
-print cads
+print (cads)
 
 cads = '''
 Esta es 
 otra
 prueba'''
 
-print cads
+print (cads)
 
 cads = "Cadenas" * 5
-print cads
+print (cads)
 
 #Concatenar
 cads2 = "Otra cosa "
 
-print cads2 + cads
+print (cads2 + cads)
 
 
-print type(cads)
+print (type(cads))
